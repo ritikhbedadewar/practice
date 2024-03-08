5726974b3c20ba50eccad1bb00173aee15c2aa8c
ritikhbedadewar <159447216+ritikhbedadewar@users.noreply.github.com>
2024-03-08 12:15:23 +0000

    ad

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 sed -i 's/simplespringapp3/changed ap name/g' $(find . -type f \( -name "*.yaml" -o -name "*.yml" -o -name "*.properties" \))
 git add .
 git commit -m "Replace 'old_text' with 'new_text'"
-git push origin master
+git push origin main
